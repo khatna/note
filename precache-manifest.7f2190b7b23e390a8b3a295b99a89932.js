02ce1ef7ccbc6dbee3f36d826662386153b0a256
khatna <khatna@github.com>
2020-01-28 00:03:27 -0500

    Updates

---
+++ precache-manifest.7f2190b7b23e390a8b3a295b99a89932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3a6de30107d5ce471ad8705cdb04b5",
+    "url": "/note/index.html"
+  },
+  {
+    "revision": "f664c529156b72471fff",
+    "url": "/note/static/css/main.9f212ff1.chunk.css"
+  },
+  {
+    "revision": "809faffdf8849c3f23a3",
+    "url": "/note/static/js/2.73974a2a.chunk.js"
+  },
+  {
+    "revision": "2425e22d6968520b7c710b95791c128a",
+    "url": "/note/static/js/2.73974a2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f664c529156b72471fff",
+    "url": "/note/static/js/main.7a6514c6.chunk.js"
+  },
+  {
+    "revision": "46dfd3be316527434739",
+    "url": "/note/static/js/runtime-main.b4d07f28.js"
+  }
+]);
